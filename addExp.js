6b382e57a4c23ededc5d984a41669aacb6fdf4cc
Stella W <46392676+stellaw1@users.noreply.github.com>
2019-10-13 10:59:32 -0700

    added js for myFood.html

---
+++ addExp.js
@@ -0,0 +1,7 @@
+const expData = '{"items": [{"name": "egg","expireDate": "1206"},{"name": "milk","expireDate": "1013"},{"name": "yogurt","expireDate": "1014"},{"name": "strawberries","expireDate": "1013"},{"name": "granola","expireDate": "1230"},{"name": "bok-choy","expireDate": "1025"},{"name": "tomato","expireDate": "1025"},{"name": "avocado","expireDate": "1014"},{"name": "lime","expireDate": "1014"},{"name": "garlic","expireDate": "1101"},{"name": "chicken","expireDate": "1015"},{"name": "onion","expireDate": "1025"}]}';
+
+var obj = JSON.parse(expData);
+
+for (var item in obj.items) {
+    document.write(obj.items[item].expireDate + "<br>");
+}
