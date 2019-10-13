bfd3a84c89b34cd63c5bb33ec6ae82e929f58439
Stella W <46392676+stellaw1@users.noreply.github.com>
2019-10-13 11:36:30 -0700

    format

--- addExp.js
+++ addExp.js
@@ -1,4 +1,4 @@
-const expData = '{"items": [{"name": "egg","expireDate": "1206"},{"name": "milk","expireDate": "1013"},{"name": "yogurt","expireDate": "1014"},{"name": "strawberries","expireDate": "1013"},{"name": "granola","expireDate": "1230"},{"name": "bok-choy","expireDate": "1025"},{"name": "tomato","expireDate": "1025"},{"name": "avocado","expireDate": "1014"},{"name": "lime","expireDate": "1014"},{"name": "garlic","expireDate": "1101"},{"name": "chicken","expireDate": "1015"},{"name": "onion","expireDate": "1025"}]}';
+const expData = '{"items": [{"name": "egg","expireDate": "12/06"},{"name": "milk","expireDate": "10/13"},{"name": "yogurt","expireDate": "10/14"},{"name": "strawberries","expireDate": "10/13"},{"name": "granola","expireDate": "12/30"},{"name": "bok-choy","expireDate": "10/25"},{"name": "tomato","expireDate": "10/25"},{"name": "avocado","expireDate": "10/14"},{"name": "lime","expireDate": "10/14"},{"name": "garlic","expireDate": "11/01"},{"name": "chicken","expireDate": "10/15"},{"name": "onion","expireDate": "10/25"}]}';
 
 var obj = JSON.parse(expData);
 
