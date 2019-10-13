7343d7edefd78675068741b8e1cdb74087b30ddb
Stella W <46392676+stellaw1@users.noreply.github.com>
2019-10-12 23:46:02 -0700

    fixed getRecipe.html

--- data.js
+++ data.js
@@ -1,8 +1,10 @@
-const data = '{"name": "Guacamole", "Ingridients": ["yogurt","strawberries","granola"], "Instruction": ["1. Cut the avocados.","2. Mince garlic.","3. Cut the lime. ", "4. Stir all ingredients."]}';
+const data = '{"name": "Guacamole","Ingridients": ["yogurt","strawberries","granola"],"Instruction": ["1. To cut the avocados, run a knife around the avocado (from top to bottom) and twist in half. Pull out and discard the pit. Using a spoon or your thumb, remove the flesh and place into a medium-sized bowl.","2. Mince or grate the garlic.","3. Cut the lime in half and squeeze both halves into the bowl with the avocado, being careful not to get any seeds in the bowl.","4. Add the garlic and salt. Using a fork gently mash each avocado half a few times then stir all ingredients together."],"Source": "https://www.theendlessmeal.com/easy-guacamole-recipe/"}';
+
 
 var obj = JSON.parse(data);
 
-document.getElementById("insert").innerHTML = obj.name;
+document.getElementById("name").innerHTML = obj.name;
+document.getElementById("source").innerHTML = obj.Source;
 
 document.write("Ingredients: <br><br>");
 for (var ingr in obj.Ingridients) {
